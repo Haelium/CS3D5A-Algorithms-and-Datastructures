9c28cc83818b282fa52d48e7f422c5287400a180
haelium <davidbourke94@gmail.com>
2016-10-26 20:06:59 +0100

    seperated hashing functions into header

--- assignment1-hashtables/hashing_functions.h
+++ assignment1-hashtables/hashing_functions.h
@@ -1,26 +1,20 @@
 /*
-hashing_functions.c -- Code demonstrating hash tables
-Author: David J. Bourke
+hashing_functions.h -- Code demonstrating hash tables
+Author: David J. Bourke, Student Number: 12304135
 Date started:   21st of October 2016
 Date submitted: 28th of October 2016
+Compile with $ gcc -o hash -std=c99 hashing_function.c
 */
 
-#include <stdio.h>
 #include <string.h>
 
-#define NUM_TEST_KEYS 25
 #define MAX_KEY_LENGTH 16
-#define HASH_TABLE_SIZE_M 28
+#define HASH_TABLE_SIZE_M 40
 
+// This is our actual hash table
 char hash_table[HASH_TABLE_SIZE_M][MAX_KEY_LENGTH];
-char test_strings[NUM_TEST_KEYS][MAX_KEY_LENGTH] = {
-    "Harkonnen", "Atreides", "Ordos", "Sandworm", "Corrino",
-    "Ecaz", "Hagal", "Fenring", "Moritani", "Mutelli",
-    "Novebruns", "Richese", "Taligari", "Vernius", "Spice",
-    "Frank Herbert", "Westwood", "Studios", "Copyright", "Infringement",
-    "Nikola Tesla", "Brotherhood", "Of NOD", "beats", "GDI"
-};
 
+// wrapper for index and # of collisions returned from probing functions
 typedef struct probe_info {
     int index;
     int collisions;
@@ -30,7 +24,7 @@ typedef struct probe_info {
 int hash_index (const char *key) {
     int index = 0;
 	for (int i = 0; i < strlen(key); i++) {
-		index += key[i] * (i*i);
+        index += key[i] * i;
 	}
     return index;
 }
@@ -39,7 +33,7 @@ int hash_index (const char *key) {
 int hash_offset (const char *key) {
     int offset = 0;
     for (int i = 0; i < strlen(key); i++) {
-        offset += key[i] ^ key[-i]; 
+        offset += key[i]; 
     }
     return offset * 2 + 1; // Always an odd number
 }
@@ -49,7 +43,7 @@ int hash_offset (const char *key) {
 // Colisions with existing keys are dealt with using linear probing
 probe_info table_probe_lp (const char *key, int table_size) {
     probe_info dh_probe_info;
-    int index = hash_index(key) % table_size;
+    int index = hash_index(key);
     int index_mod_ts; // allows for "wrapping" where (index > table_size)
     int i;  // We need i after loop for case where no empty bucket is found
     for (i = 0; i < table_size; i++) {
@@ -57,14 +51,12 @@ probe_info table_probe_lp (const char *key, int table_size) {
         // if an empty bucket is found, insert the key and return the index
         if (!hash_table[index_mod_ts][0]) {
             strcpy(hash_table[index_mod_ts], key);
-            printf("%d probes to insert: %16s,\tindex: %d\n", i, key, index_mod_ts);
             dh_probe_info.index = index_mod_ts;
             dh_probe_info.collisions = i;
             return dh_probe_info;
         }
         // if the key exists in the table already, return the index
         if (!strcmp(key, hash_table[index_mod_ts])) {
-            printf("%d probes to locate: %16s,\tindex: %d\n", i, key, index_mod_ts);
             dh_probe_info.index = index_mod_ts;
             dh_probe_info.collisions = i;
             return dh_probe_info;
@@ -81,66 +73,32 @@ probe_info table_probe_lp (const char *key, int table_size) {
 // if the key does not exist, it is added to the hash table
 // Colissions withexisting keys are dealt with using double hashing
 probe_info table_probe_dh (const char *key, int table_size) {
+    int max_loops = 100;    // Prevents infinite loops
     probe_info dh_probe_info;
-    int index = hash_index(key) % table_size;
-    int hash_off = hash_offset(key) % table_size;
+    int index = hash_index(key);
+    int hash_off = hash_offset(key);
     int index_mod_ts; // allows for "wrapping" where (index > table_size)
     int i;  // We need i after loop for case where no empty bucket is found
-    for (i = 0; table_size; i++) {
+    for (i = 0; i < max_loops; i++) {
         // h(k,i) = (f(k) + i * g(k))
         index_mod_ts = (index + i * hash_off) % table_size;
         // if an empty bucket is found, insert the key and return the index
         if (!hash_table[index_mod_ts][0]) {
             strcpy(hash_table[index_mod_ts], key);
-            printf("%d probes to insert: %16s,\tindex: %d\n", i, key, index_mod_ts);
             dh_probe_info.index = index_mod_ts;
             dh_probe_info.collisions = i;
             return dh_probe_info;
         }
         // if the key exists in the table already, return the index
         if (!strcmp(key, hash_table[index_mod_ts])) {
-            printf("%d probes to locate: %16s,\tindex: %d\n", i, key, index_mod_ts);
             dh_probe_info.index = index_mod_ts;
             dh_probe_info.collisions = i;
             return dh_probe_info;
         }
     }
-    // If no empty bucket is found, return -1
+    // If no empty bucket is found, return -1 as index
     printf("Failed to insert %s\n", key);
     dh_probe_info.index = -1;
     dh_probe_info.collisions = i;
     return dh_probe_info;
-}
-
-int main (void) {
-    probe_info temp_probe_info;
-    int total_collisions = 0;
-    // Initialise all elements of hash_table to 0
-    memcpy(hash_table, "", HASH_TABLE_SIZE_M * MAX_KEY_LENGTH);
-    printf("Probing hash table for every test string using linear probing...\n");
-    // Probe the hash table for every test_string using linear probing function
-	for ( int i = 0; i < NUM_TEST_KEYS; i++ ) {
-		probe_info temp_probe_info = table_probe_lp(test_strings[i], HASH_TABLE_SIZE_M);
-        total_collisions += temp_probe_info.collisions;
-	}
-    printf("Total number of collisions:\t %d\n", total_collisions);
-    printf("Average number of collisions:\t %lf\n", total_collisions / (double)NUM_TEST_KEYS);
-
-    total_collisions = 0;   // Reset collision counter 
-    // Reinitialise all elements of hash_table to 0
-    memcpy(hash_table, "", HASH_TABLE_SIZE_M * MAX_KEY_LENGTH);
-    printf("Probing hash table for every test string using double hashing...\n");
-    // Probe the hash table for every test_string using double hashing probe function
-	for ( int i = 0; i < NUM_TEST_KEYS; i++ ) {
-		temp_probe_info = table_probe_dh(test_strings[i], HASH_TABLE_SIZE_M);
-        total_collisions += temp_probe_info.collisions;
-	}
-    printf("Total number of collisions:\t %d\n", total_collisions);
-    printf("Average number of collisions:\t %lf\n", total_collisions / (double)NUM_TEST_KEYS);
-
-	//
-	// calculate table load here
-	//
-
-	return 0;
 }
