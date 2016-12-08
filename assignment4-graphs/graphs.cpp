0b07c8767aa0bd368f13bf3b37f120c113e1aa5c
haelium <davidbourke94@gmail.com>
2016-12-08 00:11:19 +0000

    cleanup

--- assignment4-graphs/graphs.cpp
+++ assignment4-graphs/graphs.cpp
@@ -24,13 +24,15 @@ static inline char index2ascii (int n) {
     return (n + 'A');
 }
 
+// Wrapper class for an undirected graph
 class Graph {
 public:
     Graph (void);
-    void add_link_bi (char vertex_1, char vertex_2, int cost);
+    void add_link (char vertex_1, char vertex_2, int cost);
     bool depth_first_search (char start_vertex, char end_vertex);
     bool dijkstra_search (char start_vertex, char end_vertex);
 private:
+    // Graph is stored in a adjacency matrix
     int graph[NUM_OF_NODES][NUM_OF_NODES];
 };
 
@@ -48,7 +50,7 @@ inline Graph::Graph (void) {
     }
 }
 
-inline void Graph::add_link_bi (char vertex_1, char vertex_2, int cost) {
+inline void Graph::add_link (char vertex_1, char vertex_2, int cost) {
     // TODO: Exception handling
     graph[ascii2index(vertex_1)][ascii2index(vertex_2)] = cost;
     graph[ascii2index(vertex_2)][ascii2index(vertex_1)] = cost;
@@ -87,7 +89,7 @@ inline bool Graph::depth_first_search (char start_vertex, char end_vertex) {
 
 // Container class, associates a vertex label with a cost from the source in dijktras search
 // Used as an element within a priority queue for dijkstras search
-class Vertex {
+class VertexQueueNode {
 public:
     char vertex;
     int cost_from_source;
@@ -96,30 +98,31 @@ public:
 // Comparison class, used to define how the priority queue in dijkstra's search is ordered
 class GreaterCost {
 public:
-    bool operator ()(Vertex const & vertex_1, Vertex const & vertex_2) {
+    bool operator ()(VertexQueueNode const & vertex_1, VertexQueueNode const & vertex_2) {
         return (vertex_1.cost_from_source > vertex_2.cost_from_source);
     }
 };
 
 inline bool Graph::dijkstra_search(char start_vertex, char end_vertex) {
+
     // In this implementeation, start_vertex is the "source"
     char current_v;
-    int alt;
+    int alt_route_cost;
     bool optimal_route_found = false;  // default
     // Priority queue, priority is given to Vertex objects whose cost_from_source is lowest
-    priority_queue<Vertex, vector<Vertex>, GreaterCost> unvisited;
+    priority_queue<VertexQueueNode, vector<VertexQueueNode>, GreaterCost> unvisited;
     int dist_to_vertex[NUM_OF_NODES];       // optimal known distance from source to vertex
     char prev_optimal_vertex[NUM_OF_NODES]; // previous vertex in optimal path from source
-    Vertex temp_vertex;
+    VertexQueueNode vertex_queue_node;
 
     for (char v = 'A'; v < 'A' + NUM_OF_NODES; v++) {
         dist_to_vertex[ascii2index(v)] = 99999;     // Each vertex begins with infinite cost to source
         prev_optimal_vertex[ascii2index(v)] = 0;    // 0 denotes no route to source from v
     }
     dist_to_vertex[ascii2index(start_vertex)] = 0;
-    temp_vertex.vertex = start_vertex;
-    temp_vertex.cost_from_source = 0;
-    unvisited.push(temp_vertex);
+    vertex_queue_node.vertex = start_vertex;
+    vertex_queue_node.cost_from_source = 0;
+    unvisited.push(vertex_queue_node);
 
     // While there are nodes which have not been tried, keep looping through the queue
     while (!unvisited.empty()) {
@@ -131,33 +134,35 @@ inline bool Graph::dijkstra_search(char start_vertex, char end_vertex) {
         if (current_v == end_vertex) {
                 optimal_route_found = true;
         }
-        // Loop through every neighbour, calculate the cost of taking that route and compare it to the current optimal route
+
+        // Loop through every neighbour, calculate the cost of taking that route and compare it to the current known optimal route
         for (char neighbour_v = 'A'; neighbour_v < 'A' + NUM_OF_NODES; neighbour_v++) {
             if (graph[ascii2index(current_v)][ascii2index(neighbour_v)] >= 0) { // Check that neighbour exists (-1 for no link, 0 for self)
                 // alt is the cost of the alternate route
-                alt = dist_to_vertex[ascii2index(current_v)] + graph[ascii2index(current_v)][ascii2index(neighbour_v)];
+                alt_route_cost = dist_to_vertex[ascii2index(current_v)] + graph[ascii2index(current_v)][ascii2index(neighbour_v)];
                 // If the alternate route is cheaper, this becomes the optimal route from the current vertex (current_v)
-                if (alt < dist_to_vertex[ascii2index(neighbour_v)]) {
-                    dist_to_vertex[ascii2index(neighbour_v)] = alt;             // Update the cost of optimal route for neighbour_v
+                if (alt_route_cost < dist_to_vertex[ascii2index(neighbour_v)]) {
+                    dist_to_vertex[ascii2index(neighbour_v)] = alt_route_cost;  // Update the cost of optimal route for neighbour_v
                     prev_optimal_vertex[ascii2index(neighbour_v)] = current_v;  // Update the previous node in the optimal route to neighbour_v
 
                     // push the newly considered neighbour to the queue with its dist_to_vertex
-                    temp_vertex.cost_from_source = dist_to_vertex[ascii2index(neighbour_v)];
-                    temp_vertex.vertex = neighbour_v;
-                    unvisited.push(temp_vertex);
+                    vertex_queue_node.cost_from_source = dist_to_vertex[ascii2index(neighbour_v)];
+                    vertex_queue_node.vertex = neighbour_v;
+                    unvisited.push(vertex_queue_node);
                 }
-            }
-        }
-    }
+            }   // endif (neighbour exists)
+        }   // endfor (loop through every neighbour of current_v)
+
+    }   // endwhile (priority queue not empty)
 
     // Print out the optimal route if it exists
     if (optimal_route_found) {
         char vert = end_vertex; 
         while (vert != start_vertex) {
-            printf("%c ", vert);
+            printf("%c: %d\n", vert, dist_to_vertex[ascii2index(vert)]);
             vert = prev_optimal_vertex[ascii2index(vert)];
         }
-        printf("%c\n", vert);
+        printf("%c: %d\n", vert, dist_to_vertex[ascii2index(vert)]);
     }
     // Return the result of the search (true / false)
     return optimal_route_found;
@@ -168,20 +173,20 @@ int main (void) {
     Graph test_graph;   // declaring a graph object
     // Initialising the graph as illustrated in the assignment handout
     {
-        test_graph.add_link_bi('A', 'B', 1);
-        test_graph.add_link_bi('B', 'C', 2);
-        test_graph.add_link_bi('B', 'D', 2);
-        test_graph.add_link_bi('C', 'E', 3);
-        test_graph.add_link_bi('C', 'F', 2);
-        test_graph.add_link_bi('D', 'G', 2);
-        test_graph.add_link_bi('F', 'H', 1);
-        test_graph.add_link_bi('F', 'I', 2);
-        test_graph.add_link_bi('F', 'J', 4);
-        test_graph.add_link_bi('G', 'J', 1);
-        test_graph.add_link_bi('G', 'L', 3);
-        test_graph.add_link_bi('G', 'K', 2);
-        test_graph.add_link_bi('I', 'K', 11);
-        test_graph.add_link_bi('J', 'L', 5);
+        test_graph.add_link('A', 'B', 1);
+        test_graph.add_link('B', 'C', 2);
+        test_graph.add_link('B', 'D', 2);
+        test_graph.add_link('C', 'E', 3);
+        test_graph.add_link('C', 'F', 2);
+        test_graph.add_link('D', 'G', 2);
+        test_graph.add_link('F', 'H', 1);
+        test_graph.add_link('F', 'I', 2);
+        test_graph.add_link('F', 'J', 4);
+        test_graph.add_link('G', 'J', 1);
+        test_graph.add_link('G', 'L', 3);
+        test_graph.add_link('G', 'K', 2);
+        test_graph.add_link('I', 'K', 11);
+        test_graph.add_link('J', 'L', 5);
     }
     test_graph.depth_first_search('A', 'I');
     test_graph.dijkstra_search('A', 'I');
